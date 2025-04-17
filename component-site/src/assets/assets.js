3b487eca6cae9a64a2e0d53ec840f88227060fd3
oditha4523 <odithachamod@gmail.com>
2025-04-17 17:07:56 +0530

    new changes

--- component-site/src/assets/assets.js
+++ component-site/src/assets/assets.js
@@ -1,7 +1,22 @@
-import socrates from './socrates-new2.png'
-import untitled3 from './Untitled3.png'
+
+import item1 from './item1.jpg'
+import item2 from './item2.jpg'
+import item3 from './item3.jpg'
+import item4 from './item4.jpg'
+import item5 from './item5.jpg'
+import item6 from './image6.jpg'
+import item7 from './item7.jpg'
+import item8 from './item8.jpg'
+
 
 export const assets ={
-    socrates,
-    untitled3
+
+    item1,
+    item2,
+    item3,
+    item4,
+    item5,
+    item6,
+    item7,
+    item8
 }
